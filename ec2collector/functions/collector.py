7b974ff01a6c685424b39d840efc67edb6ea3b80
hasaku63 <takuyahashimoto1988@gmail.com>
2020-05-18 17:22:47 +0900

    something action

--- ec2collector/functions/collector.py
+++ ec2collector/functions/collector.py
@@ -1,7 +1,9 @@
 import os
 import json
 from datetime import datetime
+from boto3_type_annotations.dynamodb import Table
 import boto3
+from botocore.exceptions import ClientError
 from ec2collector import settings
 
 import logging
@@ -9,38 +11,98 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-def get_instances():
-    """実行リージョンの
+def collect_instances_info():
     """
-    pass
+    実行しているリージョンに存在する EC2 すべてについて、タグを取得して返す。
+
+    ※今回の課題では、実際に EC2 API を呼び出すことはせず、タグ取得機能を模したダミー関数を提供します。
+    時間内で余力があれば、こちらも実際に動くコードに直しつつテストの切り出しに挑戦してみてください。
+
+    :return: List of dict {'InstanceId': 'i-xxxxx', 'Tags': ['tag-value1', 'tag-value2]}
+    :rtype: list
+    """
+    # ダミー実装
+    import random
+    letters = '0123456789abcdef'
+    tag_coords = [
+        {'Key': 'tag-key1', 'Value': 'value1'},
+        {'Key': 'tag-key2', 'Value': 'value2'},
+        {'Key': 'tag-key3', 'Value': 'value3'},
+        {'Key': 'require_action', 'Value':'true'}
+    ]
+    result = []
+    for i in range(5):
+        tags = random.sample(tag_coords, k=2)
+        result.append({
+            'InstanceId': 'i-' + ''.join(random.choices(letters, k=17)),
+            'Tags': tags
+        })
+    return result
+
+    # 以下、参考実装
+    # client = boto3.client('ec2', region_name=settings.AWS_REGION)
+    # 
+    # ret = client.describe_instances()
+    # instances = []
+    # try:
+    #     for reservations in ret['Reservations']:
+    #         for instance in reservations['Instances']:
+    #             instances.append({
+    #                 'InstanceId': instance['InstanceId'],
+    #                 'Tags': instance['Tags']
+    #             })
+    # 
+    # return ret
 
 
-def handler(event, context):
-    client = boto3.client('ec2', region_name=settings.AWS_REGION)
-
-    ret = client.describe_instances()
-    instances = []
+def collect(event, context):
     try:
-        for reservations in ret['Reservations']:
-            for instance in reservations['Instances']:
-                instances.append({
+        instances = collect_instances_info()
+    except Exception as e:
+        log.error(e)
+        raise e
+
+    try:
+        dynamo = boto3.resource('dynamodb')
+        table: Table = dynamo.Table(settings.TABLE_NAME)
+        with table.batch_writer(overwrite_by_pkeys=['InstanceId']) as batch:
+            # 参考: https://dev.classmethod.jp/articles/lambda-python-dynamodb/
+            for instance in instances:
+                batch.put_item(Item={
                     'InstanceId': instance['InstanceId'],
                     'Tags': instance['Tags']
                 })
-
     except Exception as e:
         log.error(e)
         raise e
+    
+    return instances
 
+
+def something_action(event, context):
+    items = []
     try:
-        # TODO: dynamodb に突っ込むためのコードを書く
-        pass
-    except Exception as e:
-        log.error(e)
+        dynamo = boto3.resource('dynamodb')
+        table: Table = dynamo.Table(settings.TABLE_NAME)
+        response = table.scan()
+        items = response['Items']
+    except ClientError as e:
+        log.error(e.response['Error']['Message'])
         raise e
+    result = []
+    for item in items:
+        tag_keys = [tag['Key'] for tag in item['Tags']]
+        if 'require_action' in tag_keys:
+            log.info(f"found required action: {item['InstanceId']}")
+            result.append(item)
+    for instance in result:
+        # do something action
+        log.info(f"something awesome: {json.dumps(instance)}")
+    return result
+
 
 if __name__ == '__main__':
     # for test run in based on your local environemnt
-    ret = handler({}, {})
-    print(ret)
+    ret = collect({}, {})
+    print(json.dumps(ret, indent=2))
 
--- ec2collector/settings.py
+++ ec2collector/settings.py
@@ -3,4 +3,5 @@ import dotenv
 
 dotenv.load_dotenv()
 
-AWS_REGION = os.environ.get('AWS_REGION', '')
+AWS_REGION = os.environ.get('AWS_REGION')
+TABLE_NAME = os.environ.get('TABLE_NAME')
