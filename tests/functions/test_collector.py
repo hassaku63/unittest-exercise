6db7c7efa6835b0d63feb6fc95622897f9b9beec
hasaku63 <takuyahashimoto1988@gmail.com>
2020-05-18 23:17:28 +0900

    add tests context

---
+++ tests/functions/test_collector.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+from ec2collector.functions.collector import (
+    collect,
+    something_action
+)
+
+
+class TestCollect(TestCase):
+    def test_collect(self):
+        pass
+
+class TestSomethingAction(TestCase):
+    def test_something_action(self):
+        pass
